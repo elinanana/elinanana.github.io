0168ee7a543b5ebdda2885515e37e814252491d9
Elina <77745290+elinanana@users.noreply.github.com>
2024-02-09 23:01:08 +0100

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 8080;
@@ -8,29 +7,12 @@ const port = 8080;
 // Serve static files from the 'static' directory
 app.use('/static', express.static(path.join(__dirname, 'static')));
 
-
 // Serve the 'index.html' file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
-// use cookieParser to ensure that /dashboard is accessible only to authenticated user
-app.use(cookieParser());
-
-// Middleware to check session cookie
-const authenticateSession = (req, res, next) => {
-    const sessionCookie = req.cookies.jwt;
-
-    if (!sessionCookie) {
-        // Redirect to login if session cookie is not present
-        return res.redirect('/');
-    }
-
-    next();
-};
-
-app.get('/dashboard', authenticateSession, (req, res) => {
+app.get('/dashboard', (req, res) => {
     res.sendFile(path.join(__dirname, 'dashboard.html'));
 });
 
